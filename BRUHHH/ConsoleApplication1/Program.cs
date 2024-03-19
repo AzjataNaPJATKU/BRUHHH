38dfd6800de3aeb0e3453543c351d950a961ca60
s25917 <s25917@pjwstk.edu.pl>
2024-03-19 16:14:02 +0100

    COMMIT BRO

---
+++ BRUHHH/ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApplication1;
+
+public class BRUHHH
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Console.WriteLine("BRUH!");
+        }
+    }
+}
